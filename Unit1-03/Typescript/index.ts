e91b773fca7206f760beb13103232e9d81a890fe
Evan Beaudoin <evan.beaudoin@mths.ca>
2024-02-26 14:01:09 +0000

    Init

--- Unit1-03/Typescript/index.ts
+++ Unit1-03/Typescript/index.ts
@@ -6,44 +6,45 @@
  * Since:   2024-02-16
  */
 
-import { createPrompt } from 'bun-promptx';
+import { createPrompt } from 'bun-promptx'
 
 // variables
-let cookTimes: { [key: string]: number } = {
+//let cookTimes: { [key: string] number } = 
+let cookTimes = {
   "pizza": 45,
   "soup": 105,
   "sub": 60
-};
+}
 
 // input
-var itemString = createPrompt("Enter the food you want to heat: ");
-var amountString = createPrompt("Enter the quantity of food you want to heat: ");
+var itemString = createPrompt("Enter the food you want to heat: ")
+var amountString = createPrompt("Enter the quantity of food you want to heat: ")
 
-var item = itemString.value ?? "";
-var amount = parseInt(amountString.value || "-1");
+var item = itemString.value ?? ""
+var amount = parseInt(amountString.value || "-1")
 
 
 // process and error handling
 if (item in cookTimes && (amount > 0 && amount <= 3 )) {
-    var increaseMulitplier: number = 1;
+  var increaseMulitplier = 1
 
-    if (amount == 2) {
-      increaseMulitplier = 1.5;
-    }
-    else if (amount == 3) {
-      increaseMulitplier = 2;
-    }
-    var cookTime = cookTimes[item] * increaseMulitplier;
-    var minutes = Math.floor(cookTime / 60);
-    var seconds = cookTime % 60;
+  if (amount == 2) {
+    increaseMulitplier = 1.5
+  }
+  else if (amount == 3) {
+    increaseMulitplier = 2
+  }
+  var cookTime = cookTimes[item] * increaseMulitplier
+  var minutes = Math.floor(cookTime / 60)
+  var seconds = cookTime % 60
 
-    console.log("Cook time is " + minutes + " minutes and " + seconds + " seconds.");
+  console.log("Cook time is " + minutes + " minutes and " + seconds + " seconds.")
 }
 else {
-  console.log("\nInvalid Input");
+  console.log("\nInvalid Input")
 }
 
 
-console.log("\nDone.");
+console.log("\nDone.")
 
 
